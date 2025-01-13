f036862851aa2deb7e91907dd7bb4eeddda07e77
isupreetk <supreet97@gmail.com>
2025-01-13 12:27:28 -0800

    changes url for resume

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -42,7 +42,7 @@ function Header() {
               <a
                 target="_blank"
                 rel="noreferrer noopener"
-                href="https://drive.google.com/file/d/1vB2gmlw1S0hWUXC3VYCIAvJMnLIR8vk3/view?usp=sharing"
+                href="https://drive.google.com/file/d/1bK74bTogOiSYNHtOC0ysSTJMiFZrYDer/view?usp=sharing"
                 class="header__link"
               >
                 Resume
